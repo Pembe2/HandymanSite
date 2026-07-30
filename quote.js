98e279de6148655a04de43302cff1cfc0ba8fcfa
Mark Pemberton <markdpemberton@gmail.com>
2026-07-30 20:57:34 +0200

    Update site after rename of repository

--- quote.js
+++ quote.js
@@ -1,5 +1,5 @@
 // Replace this address with the email address where quote requests should be sent.
-const BUSINESS_EMAIL = "markdpemberton@gmail.com";
+const BUSINESS_EMAIL = "NorthBeachPS.llc@gmail.com";
 
 const form = document.getElementById("quoteForm");
 const formMessage = document.getElementById("formMessage");
